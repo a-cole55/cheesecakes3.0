27375366186dc9107f0bfad63b9291cfcf43816f
a-cole55 <arniqua_c@yahoo.com>
2023-10-22 15:32:48 -0500

    Add homepage content and stylesheet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
-import logo from './logo.svg';
 import './App.css';
+import HomeLogo from "./assets/default-min.png";
+// import "./Home.css";
+// import { Link } from "react-router-dom";
+// import { motion } from "framer-motion";
+import HomeImg from "./assets/bourbonpecan-min.jpg";
+import Home2 from "./assets/strawberrys-min(3).jpg";
+import heart from "./assets/heart.png";
+import box from "./assets/packages-icon-13.png";
+import cheesecakeIcon from "./assets/cheesecake-icon.png";
+import MenuPreview from "./components/MenuPreview";
+import pb from "./assets/pb.png";
+import pb2 from "./assets/pb2.png"
+import summer from "./assets/summer.png";
+import summer2 from "./assets/summer2.png";
+import special from "./assets/IMG_3073.jpg";
+
+
+// const transition = {duration: 4, ease: [0.43, 0.13, 0.23, 0.96]};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <div 
+            class="landing">
+            {/* initial={{ opacity: 0.1}}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0}}
+            transition={transition}> */}
+                <img class="homeLogo" src={HomeLogo} alt="logo"></img>
+        <p
+            // initial={{ y: "-70%"}}
+            // animate={{ y: "-45%", transition: {delay: 0.4, duration: 1, ease: [0.43, 0.13, 0.23, 0.96]}}}
+            // exit={{ opacity: 0}}
+            // transition={transition}
+            >Welcome Cheesecake Lover! I look forward to providing you with the best quality cheesecakes you've ever experienced.</p>
+        <img class="landing-image right" src={Home2} alt="cheesecake img"></img>
+        <img class="landing-image left" src={HomeImg} alt="cheesecake img"></img>
+        
+        {/* <div className="orderNow">
+            <div className="orderNowBTN">
+            <button className="order-BTN">Order Now</button>
+            </div>
+        </div>
+        <div className="menuPreview">
+            <MenuPreview src={pb} 
+                alt="reeses cheesecake" 
+                caption="Combining all the creamy, salty, buttery, fruity, unforgettably tasty flavors..."
+                src2={pb2}/>
+            <MenuPreview 
+                src={summer2} 
+                alt="strawberry lemonade cheesecake" 
+                caption="Summer calls for cheesecake. And lil’ pocket-size cheesecakes too. Savor every slice of summer with..."
+                src2={summer}/>
+            <MenuPreview 
+                src={pb} 
+                alt="reeses cheesecake" 
+                caption="Sink your teeth into the mini life."
+                src2={pb2}/>
+        </div>
+
+        <div className="infoSection">
+            <h2>Why Choose Cheesecakes By Chelsea?</h2>
+            <div className="iconSection">
+                <div className="infoItems">
+                    <img src={cheesecakeIcon} alt="cheesecake icon" id="first-section" />
+                    <h3>Custom Cheesecakes</h3>
+                </div>
+                <div className="infoItems">
+                    <img src={heart} alt="heart icon" id="heartIcon"></img>
+                    <h3>Made With <br></br>Love</h3>
+                </div>
+                <div className="infoItems">
+                    <img src={box} alt="package icon"></img>
+                    <h3>Thoughtfully Packaged</h3>
+                </div>
+            </div>
+        </div>
+        <div className="specialsPreviewContainer">
+            <div className="specialsPreview">
+                <h4>Try It All This Summer.</h4>
+                <p>Why stop at one, when you can have ‘em all. From crunch lovers to ooey-gooey connoisseurs, there’s a treat for everyone.</p>
+            </div>
+            <img whileHover={{scale:1.1}} src={special} alt="turtle cheesecake cupcakes" />
+    
+        </div> */}
+    
+        </div>
   );
 }
 
